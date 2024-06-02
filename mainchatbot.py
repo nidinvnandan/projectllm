5514f6bec019b2177fed3bd40098ae9a92981c4f
NIDIN V NANDAN <128630742+nidinvnandan@users.noreply.github.com>
2024-06-03 01:40:08 +0530

    Update mainchatbot.py

--- mainchatbot.py
+++ mainchatbot.py
@@ -35,7 +35,6 @@ os.environ["GOOGLE_API_KEY"] = 'AIzaSyA7u2G1y06P1KFfGuT1gVhj-8JbV409NdM'
 os.environ["COHERE_API_KEY"] = 'ErFPP679z7c4wPQLv8zp63JRblE8N2I4N1LxGmUQ'
 llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.7)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-@st.cache_resource
 def load_and_split_pdf(pdf_path):
     loader = PyPDFLoader("ZETA_CORPORATION.pdf")
 
